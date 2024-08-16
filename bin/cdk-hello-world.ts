99fde6c39155b80d5f666aa864516ec90532e9e9
fonchu <hintekk@fonchu.local>
2024-08-16 18:11:52 +0100

    initial commit

--- bin/cdk-hello-world.ts
+++ bin/cdk-hello-world.ts
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkHelloWorldStack } from '../lib/cdk-hello-world-stack';
+import { CdkHelloWorldStack, RepositoryStack } from '../lib/cdk-hello-world-stack';
 
 const app = new cdk.App();
-new CdkHelloWorldStack(app, 'CdkHelloWorldStack', {
+
+const repoStack = new RepositoryStack(app, "repoStackName", {
+  // env: { account, region },
+})
+new CdkHelloWorldStack(app, 'CdkHelloWorldStack', repoStack.repository, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -1,16 +1,94 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+// Import Lambda L2 construct
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as apigw2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as ecr from "aws-cdk-lib/aws-ecr"
+import { HttpAlbIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+export const PREFIX = "eda-ecs";
 export class CdkHelloWorldStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, ecrRepository: ecr.Repository, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "EdaVpc", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      maxAzs: 2, // Default is all AZs in region
+      vpcName: `${PREFIX}-vpc`,
+      restrictDefaultSecurityGroup: false
+    });
+
+    const cluster = new ecs.Cluster(this, "EdaCluster", {
+      vpc: vpc,
+      clusterName: `${PREFIX}-cluster`
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "EdaFargateService", {
+      cluster: cluster, // Required
+      cpu: 256, // Default is 256
+      serviceName: `${PREFIX}-service`,
+      loadBalancerName: `${PREFIX}-alb`,
+      desiredCount: 2, // Default is 1
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromAsset("./container/"),    //.fromRegistry("amazon/amazon-ecs-sample"),
+        environment: {
+          ENV_VAR_1: "value1",
+          ENV_VAR_2: "value2",
+        },
+        containerPort: 80
+      },
+      memoryLimitMiB: 512, // Default is 512
+      publicLoadBalancer: false // Default is true
+    });
+
+// Add Scalling
+    const scaling = service.service.autoScaleTaskCount({ maxCapacity: 5, minCapacity: 1 });
+    scaling.scaleOnCpuUtilization("CpuScaling", { targetUtilizationPercent: 70 }); // default cooldown of 5 min
+    scaling.scaleOnMemoryUtilization("RamScaling", { targetUtilizationPercent: 70 }); // default cooldown of 5 min
+    
+    service.targetGroup.configureHealthCheck({
+      path: "/"
+    })
+
+    const httpApi = new apigw2.HttpApi(this, "HttpApi", { apiName: `${PREFIX}-api` });
+
+    httpApi.addRoutes({
+      path: "/",
+      methods: [apigw2.HttpMethod.GET],
+      integration: new HttpAlbIntegration("AlbIntegration", service.listener)
+    })
+
+
+    // Define the Lambda function resource
+    const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
+      code: lambda.Code.fromAsset('lambda'), // Points to the lambda directory
+      handler: 'hello.handler', // Points to the 'hello' file in the lambda directory
+    });
+
+    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: helloWorldFunction,
+      proxy: false,
+    });
+
+    // Define the '/hello' resource with a GET method
+    const helloResource = api.root.addResource('hello');
+    helloResource.addMethod('GET');
+  }
+}
+
+
+export class RepositoryStack extends cdk.Stack {
+  repository: ecr.Repository;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkHelloWorldQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const repository = new ecr.Repository(this, "Repository", { repositoryName: `${PREFIX}-repository` });
+    this.repository = repository;
   }
 }
