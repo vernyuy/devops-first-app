297782e1bdc47079377c1ad444a44bd80770ecf5
fonchu <hintekk@fonchu.lan>
2024-08-27 01:37:26 +0100

    updates

--- bin/cdk-hello-world.ts
+++ bin/cdk-hello-world.ts
@@ -6,4 +6,4 @@ import { DevopsFirstAppStack } from '../lib/devops-first-app-stack'
 const app = new cdk.App();
 
 // const repoStack = new RepositoryStack(app, "repoStackName", {})
-new DevopsFirstAppStack(app, 'DevopsFirstAppStack',  {});
+new DevopsFirstAppStack(app, 'DevopsAppStack',  {});
