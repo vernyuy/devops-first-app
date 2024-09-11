6a2260b664723fd8d08b4e42ece1129968b04de8
fonchu <hintekk@fonchu.local>
2024-09-11 11:16:30 +0100

    updates

--- services/nodejs-app-2/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-const app = express();
-
-app.get("/", (request, response) => {
-  response.send(`Welcome to second micro-service`)
-})
-
-const PORT = 80
-app.listen(PORT, () => console.log(`The app is listening on port ${PORT}.`));
